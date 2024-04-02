17acb7f6bb58db04a42b9134dad000c2e070b1f5
DevHN504 <155341765+DevHN504@users.noreply.github.com>
2024-04-01 21:19:11 -0600

    customize rows and columns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DxButtonModule, DxDataGridModule } from 'devextreme-angular';
+import { Customer, customers } from '../model/customer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DxDataGridModule, DxButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'color-grid-app';
+export class AppComponent implements OnInit {
+  title: string = 'color-grid-app';
+  datasource: Customer[] = [];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.datasource = customers;
+  }
+
+  addColor(e: any): void{
+    if (e.rowType === 'data' && e.column.dataField === "state") {
+      const state = e.data.state;
+
+      switch (state) {
+        case 'Arkansas':
+          /*OPTION #1 */
+          e.cellElement.style.cssText = "background-color: #FFFF00";
+          break;
+        case 'Georgia':
+          /*OPTION #2 */
+          e.cellElement.className = 'stateGeorgia';
+          break;
+        case 'Minnesota':
+          /*OPTION #3 */
+          e.cellElement.classList.add('stateMinnesota');
+          break;
+        default:
+          break;
+      }
+    }
+  }
 }
